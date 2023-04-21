d57ee11e61512a5c6c4afcfdec426928a1b17c83
git-anik-3 <anik.devxhub@gmail.com>
2023-04-21 19:35:15 +0600

    meeting content download as pdf

--- src/components/MeetingDetails.js
+++ src/components/MeetingDetails.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getMeetingDetailsAction } from "../redux/actions/meetingActions";
 import { getAllMembersAction } from "../redux/actions/memberActions";
 import parse from "html-react-parser";
 import dateFormat from "dateformat";
+import ReactToPrint from "react-to-print";
 
 const MeetingDetails = () => {
   const { departmentId, committeeId, meetingId } = useParams();
@@ -22,18 +23,26 @@ const MeetingDetails = () => {
     if (members.length === 0) dispatch(getAllMembersAction(departmentId));
   }, [dispatch, departmentId, members]);
 
+  const pdfRef = useRef(null);
+
   return (
     <div className="container-fluid" key={meeting?.id}>
       <div className="row pt-2">
-        <div className="col-md-10">
+        <div className="col-md-10 col-lg-10 col-sm-10 my-4">
           <h1>{meeting?.title}</h1>
           <h3 className="text-decoration-underline">Description</h3>
           {meeting?.description && <div>{parse(meeting?.description)}</div>}
           <br />
           <h3 className="text-decoration-underline">Content</h3>
-          {meeting?.content && <div>{parse(meeting?.content)}</div>}
+          {meeting?.content && (
+            <div ref={pdfRef}>{parse(meeting?.content)}</div>
+          )}
+          <ReactToPrint
+            content={() => pdfRef.current}
+            trigger={() => <button className="btn btn-primary">Get PDF</button>}
+          />
         </div>
-        <div className="col-md-2">
+        <div className="col-md-2 col-lg-2 col-sm-2">
           <h3 className="text-decoration-underline">Meeting Time</h3>
           <p>
             {dateFormat(
